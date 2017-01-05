0cb7df6c38fe003acf630b85eba0a3b5730a9f61
Alexis <alexisvt@gmail.com>
2017-01-05 15:28:46 -0600

    replacing deprecated way of loading a module

--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -2,7 +2,7 @@
 const button = document.createElement('button');
 button.innerText = 'Click me';
 button.onclick = () => {
-  System.import('./image_viewer').then(module => { module.default(); });
+  import('./image_viewer').then(module => { module.default(); });
 };
 
 document.body.appendChild(button);
