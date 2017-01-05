a29f9a8f9d7730a37f8ac09eee0d5b3e6e7b414b
Alexis <alexisvt@gmail.com>
2017-01-04 18:29:53 -0600

    initial commit

---
+++ js_modules/src/index.js
@@ -0,0 +1,9 @@
+// @flow
+import {sum} from './sum';
+
+// just execute it inside in our project
+import './image_viewer';
+
+const total = sum(10,5);
+
+console.log(total);
---
+++ js_modules/src/sum.js
@@ -0,0 +1,5 @@
+// @flow
+
+const sum = (a, b) => a + b;
+
+export { sum };
